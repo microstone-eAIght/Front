c0fc2426224fe86b50a58591e290b37f4e419735
juhyeok <wngur3033@naver.com>
2024-09-20 15:11:27 +0900

    logout 기능 추가 및 config 시크릿 키 파일추가

--- app.py
+++ app.py
@@ -1,28 +1,34 @@
-from flask import Flask
+from flask import Flask, session
 from models import db
 from controllers.employee_bp import employee_bp
 from controllers.index_bp import index_bp
 from controllers.login_bp import login_bp
 from controllers.signup_bp import signup_bp
 from controllers.storage_bp import storage_bp
-
+from config import Config  # config.py 임포트
+from flask_sqlalchemy import SQLAlchemy  # Flask-SQLAlchemy 임포트
 import mysql.connector
 
 app = Flask(__name__)
+app.config.from_object(Config)  # Config 클래스에서 설정 불러오기
 
-app.secret_key = '0106'  # 세션 암호화를 위한 시크릿 키 설정 //원래는 config.py에 이 코드 보관
+
+@app.route('/logout', methods=['POST'])
+def logout():
+    session.pop('user_id', None)  # 세션에서 사용자 ID 제거
+    return '', 204  # No Content 응답 반환
 
 # MySQL 데이터베이스에 직접 연결
-connection = mysql.connector.connect(
-    host='10.1.3.246',
-    user='user6',
-    password='1234',
-    database='mydb',
-    auth_plugin='mysql_native_password'  # 인증 방식 설정
-)
+#connection = mysql.connector.connect(
+ #   host='10.1.3.246',
+  #  user='user6',
+   # password='1234',
+    #database='mydb',
+    #auth_plugin='mysql_native_password'  # 인증 방식 설정
+#)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://user6:1234@10.1.3.246/mydb'    #원격 db
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:1234@127.0.0.1/mydb'    #로컬 db
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://user6:1234@10.1.3.246/mydb'    #원격 db
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:1234@127.0.0.1/mydb'    #로컬 db
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # SQLAlchemy와 Flask 앱 연동
