953be39137d90aac33f93cad8c106bb8c21bc866
harryer68 <harryer@naver.com>
2024-10-07 16:54:49 +0900

    로그아웃 구현

--- app.py
+++ app.py
@@ -11,7 +11,6 @@ from controllers.reba_button_bp import reba_button_bp
 from controllers.analysis_bp import analysis_bp
 from controllers.admin_bp import admin_bp
 from config import Config  # config.py 임포트
-from processes.porcess_manager import terminate_processes
 from AI.database_handler import get_image_data
 
 from flask import Flask, send_from_directory, jsonify #블루프린트 해주삼
@@ -47,8 +46,6 @@ app.register_blueprint(recent_img_bp)
 app.register_blueprint(analysis_bp)
 app.register_blueprint(admin_bp)
 
-# 서버 종료 시 실행 중인 백그라운드 프로세스를 종료
-atexit.register(terminate_processes)
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=5000, debug=True)
