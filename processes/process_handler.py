7a78e4b9802221c9153115047bfe31291c944e48
harryer68 <harryer@naver.com>
2024-10-09 12:25:43 +0900

    스크립트 수정

--- processes/process_handler.py
+++ processes/process_handler.py
@@ -1,11 +1,12 @@
 # signal_handler.py
 import signal
 import sys
-from process_manager import stop_all_scripts
+from processes.process_manager import stop_all_scripts
 
 # SIGINT 핸들러 설정
 def signal_handler(sig, frame):
     stop_all_scripts()  # 모든 스크립트를 종료
+    print("스크립트 전부 종료")
     sys.exit(0)
 
 # 시그널 핸들러 등록
